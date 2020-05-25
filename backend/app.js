6d9afe476fbf8eb90b3c730c22752e8bdeab7701
Cezar Augusto Bononi <46329971+cezarbononi@users.noreply.github.com>
2020-05-25 20:31:08 -0300

    terminando a integração com backend e adicionando a função delete

--- backend/app.js
+++ backend/app.js
@@ -30,13 +30,15 @@ app.use((req, res, next) => {
 });
 
 app.post('/api/posts', (req, res, next) => {
-  let post = new Post({
+  const post = new Post({
     title: req.body.title,
     content: req.body.content
   });
-  post.save();
-  res.status(201).json({
-    message: 'Post added successfully!'
+  post.save().then(createdPost => {
+    res.status(201).json({
+      message: 'Post added successfully!',
+      postId: createdPost._id
+    });
   });
 });
 
@@ -49,6 +51,13 @@ app.get('/api/posts', (req, res, next) => {
         posts: documents
       });
     });
+});
+
+app.delete('/api/posts/:id', (req, res, next) => {
+ Post.deleteOne({_id: req.params.id}).then(result => {
+   console.log(result);
+   res.status(200).json({ message: 'Post Deleted!'});
+  })
 });
 
 module.exports = app;
